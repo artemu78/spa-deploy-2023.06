1db8c714f412e11df4254939fa98c1b51645ca21
artemu78 <artemreva79@gmail.com>
2024-02-16 17:34:24 +0000

    Deploying to gh-pages from @ artemu78/spa-deploy-2023.06@92ccd3ff2c8798ee7fcc4171f4b1039da964731f 🚀

--- bundle.js
+++ bundle.js
@@ -1,1 +1,1 @@
-!function(){function t(t){const e=window.location.href;document.querySelector("#app").textContent=t?e.includes("#")?e.replace(/#.*/,t):e+t:window.location.href}document.body.addEventListener("click",(e=>{e.target.matches("a")&&t(e.target.hash)})),t()}();
+!function(){function t(t){const e=window.location.href;document.querySelector("#app").textContent=t?e.includes("#")?e.replace(/#.*/,t):e+t:window.location.href}document.body.addEventListener("click",(e=>{e.target.matches("a")&&(e.preventDefault(),history.pushState(null,"",e.target.href),t())})),t()}();
