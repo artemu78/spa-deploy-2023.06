92ccd3ff2c8798ee7fcc4171f4b1039da964731f
Artem Reva <artem_reva@epam.com>
2024-02-16 20:33:20 +0300

    Step 1

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ function render(hash?: string) {
   const href = window.location.href;
 
   document.querySelector("#app")!.textContent = hash
-      ? href.includes("#")
-          ? href.replace(/#.*/, hash)
-          : href + hash
-      : window.location.href;
+    ? href.includes("#")
+      ? href.replace(/#.*/, hash)
+      : href + hash
+    : window.location.href;
 }
 
 // if (PRODUCTION) {
@@ -16,9 +16,9 @@ function render(hash?: string) {
 
 document.body.addEventListener("click", (ev) => {
   if ((ev.target as HTMLElement).matches("a")) {
-    const hash = (ev.target as HTMLAnchorElement).hash;
-
-    render(hash);
+    ev.preventDefault();
+    history.pushState(null, "", (ev.target as HTMLAnchorElement).href);
+    render();
   }
 });
 
